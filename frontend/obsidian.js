fa1767bb67b161f57d590f6aec89f9b1a70b7fdc
Mario Diaz <mariodiar@gmail.com>
2017-04-03 02:33:33 -0500

    Generate JS code with blockly

--- frontend/obsidian.js
+++ frontend/obsidian.js
@@ -1,3 +1,12 @@
 var workspace = Blockly.inject('blocklyDiv',
         {media: '../dependencies/blockly/media/',
          toolbox: document.getElementById('toolbox')});
+    Blockly.Xml.domToWorkspace(document.getElementById('startBlocks'),
+                               workspace);
+
+    function showCode() {
+      // Generate JavaScript code and display it.
+      Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+      var code = Blockly.JavaScript.workspaceToCode(workspace);
+      alert(code);
+    }
